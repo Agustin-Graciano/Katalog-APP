f6f0ba9525e13bbfea5d79d4db0275578924ab67
Agustin-Graciano <agusnachogracianox@gmail.com>
2023-01-15 02:44:46 +0100

    Nav bar changed to English

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -2,18 +2,18 @@
     <div class="bg-white px-4 py-2">
       <div class="flex justify-center">
         <div class="right-border mt-20 mb-20">
-          <router-link to="/PSAKort" class="inline-block px-4 mt-2 mb-2 text-blue font-bold hover:text-black">PSA Kort</router-link>
-          <router-link to="/Opbevaring" class="inline-block px-4 mt-2 mb-2 text-blue font-bold hover:text-black">Opbevaring</router-link>
-          <router-link to="/priser" class="inline-block px-4 mt-2 mb-2 text-blue font-bold hover:text-black">Priser</router-link>
+          <router-link to="/PSA Cards" class="inline-block px-4 mt-2 mb-2 text-blue font-bold hover:text-black">PSA Cards</router-link>
+          <router-link to="/Card Holders" class="inline-block px-4 mt-2 mb-2 text-blue font-bold hover:text-black">Card Holders</router-link>
+          <router-link to="/Prises" class="inline-block px-4 mt-2 mb-2 text-blue font-bold hover:text-black">Prises</router-link>
         </div>
         <div>
           <router-link to="/"><img src="https://cdn.shopify.com/s/files/1/0558/2749/7135/files/160951771_137318954966106_5380319641252698276_n_360x.png?v=1616866138" width="150" class="rounded-full" alt="logo"/></router-link>
         </div>
         <div class="left-border mt-20 mb-20">
-          <router-link to="/vurdering" class="inline-block px-4 mt-2 mb-2 text-blue font-bold hover:text-black">Få en gratis levering</router-link>
+          <router-link to="/Valuation" class="inline-block px-4 mt-2 mb-2 text-blue font-bold hover:text-black">Get a free valuation</router-link>
           <router-link to="/FAQ"
             class="inline-block px-4 mt-2 mb-2 text-blue font-bold hover:text-black">FAQ</router-link>
-          <router-link to="/Kontakt" class="inline-block px-4 mt-2 mb-2 text-blue font-bold hover:text-black">kontakt</router-link>
+          <router-link to="/Contact" class="inline-block px-4 mt-2 mb-2 text-blue font-bold hover:text-black">Contact</router-link>
         </div>
       </div>
     </div>
